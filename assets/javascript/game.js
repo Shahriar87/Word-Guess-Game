7a993fe7f6f00a84347b656053ea00d42394c60e
Shahriar87 <shahriar.firoz@gmail.com>
2018-09-03 21:30:48 -0500

    sixth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ var wordBandName10 = "DEEP PURPLE".split('');
 
 var computerWords = [wordBandName1, wordBandName2, wordBandName3, wordBandName4, wordBandName5, wordBandName6, wordBandName7, wordBandName8, wordBandName9, wordBandName10];
 
-var guessesLeft = 13;
+var guessesLeft = 9;
 var wins = 0;
 var listOfChoices = [];
 var dash = [];
@@ -24,6 +24,19 @@ var getUserText = document.getElementById("user-text");
 var getWord = document.getElementById("word");
 var getResult = document.getElementById("result");
 var getWin = document.getElementById("win");
+var getImg = document.getElementById("hangmanImg");
+
+var img8 = "assets/images/Hangman miss1.png";
+var img7 = "assets/images/Hangman miss2.png";
+var img6 = "assets/images/Hangman miss3.png";
+var img5 = "assets/images/Hangman miss4.png";
+var img4 = "assets/images/Hangman miss5.png";
+var img3 = "assets/images/Hangman miss6.png";
+var img2 = "assets/images/Hangman miss7.png";
+var img1 = "assets/images/Hangman miss8.png";
+var img0 = "assets/images/Hangman miss9.png";
+
+var img = [img0, img1, img2, img3, img4, img5, img6, img7, img8];
 
 getGuess.textContent = guessesLeft;
 getUserText.textContent = ":";
@@ -33,8 +46,9 @@ getUserText.textContent = ":";
 
 
 function compGuess(){            // Reset the Game
-    
-    guessesLeft = 13;
+
+    // getImg.src="assets/images/Hangman Empty.png"
+    guessesLeft = 9;
     listOfChoices = []; 
     dash = [];
     computerGuess = computerWords[Math.floor(Math.random()* computerWords.length)];
@@ -54,9 +68,15 @@ function compGuess(){            // Reset the Game
   
 };
 
+// document.onkeyup = function startGame(event){
+//     var keyStart = event.keyCode;
+//     if (ketStart === 32){
+//         compGuess(); 
+//     }
+    
+// }
 
 compGuess(); 
-
 
 
 
@@ -77,6 +97,7 @@ document.onkeyup = function hangman(event) {
 
             } else if (guessesLeft > 0){                        // Else, reduce guess by 1
                 guessesLeft --;
+                changeImg();
                 listOfChoices.push(userChoice);             
             }    
             getUserText.textContent = listOfChoices;       
@@ -98,15 +119,22 @@ document.onkeyup = function hangman(event) {
             }
         }
 
+        function changeImg(){
+            var j = guessesLeft;
+            getImg.src=img[j];
+        }
+
         if (dash.indexOf('-')===-1){                            // Winning!
             getResult.innerHTML = "You Win!"; 
             wins ++;        
+            // startGame();
             compGuess();
         }
         
         if (guessesLeft===0){
             getResult.innerHTML = "You Lose! <br/> Game Reset.";                // Losing!
             wins = 0;       
+            // startGame();
             compGuess();
         }
 
